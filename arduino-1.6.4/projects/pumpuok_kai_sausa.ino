88f95cf49b8973823fe13f7fd2b9d3b80ae6be76
Albertas <albertas.gimbutas@mii.vu.lt>
2015-07-03 00:02:33 +0300

    Add Arduino program, which pumps watter, when moisture sensor shows dry soil.

---
+++ arduino-1.6.4/projects/pumpuok_kai_sausa.ino
@@ -0,0 +1,28 @@
+// the setup function runs once when you press reset or power the board
+int sensorPin = 14;
+int humidity_value = 0;
+int humidity_low = 70;   // max 710
+int times_pumped = 0;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);              // wait for a second
+  humidity_value = analogRead(sensorPin);
+  Serial.println(humidity_value);
+  times_pumped = 0;
+  while ((humidity_value <= humidity_low) && (times_pumped < 5)) {
+    digitalWrite(13, HIGH);
+    delay(1000);
+    humidity_value = analogRead(sensorPin);
+    times_pumped += 1;
+    Serial.println(humidity_value);
+  };
+}
